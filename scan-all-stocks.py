f2a39a38ecab86372012d70e32e877e54548fecf
Yubin <nkyubin@gmail.com>
2016-01-04 08:57:40 +0800

    filter ValueError exception for those have cached prices

--- scan-all-stocks.py
+++ scan-all-stocks.py
@@ -1,14 +1,17 @@
 # load stock-signal.py # load it by manual
 # load load-code.py # load it by manual
 import time
+import random
 
 stocks=load_code_ex()
 
 for i in range(stocks[0].count()):
     try:
         stock_signal_w_new(stocks[0][i])
-        time.sleep(1)
+    except ValueError, ve:
+        continue
     except Exception, ex:
-        print ex
+        print stocks[0][i],stocks[1][i],ex
+        time.sleep(random.randint(1,5))
 
     
