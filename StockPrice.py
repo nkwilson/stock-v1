adb726a6f845d15e30303ae6188c192634f12e45
Yubin <nkyubin@gmail.com>
2016-01-10 18:48:11 +0800

    if no cache then retrieve from web

--- StockPrice.py
+++ StockPrice.py
@@ -23,7 +23,7 @@ def StockPrice_w(stock):
     url='http://real-chart.finance.yahoo.com/table.csv?s=%s&a=0&b=1&c=2015&d=11&e=11&f=2016&g=w&ignore=.csv' % stock
     filename='%sw.csv' % stock
     if not os.path.isfile(filename):
-        raise ValueError,'invalid argument'
+#        raise ValueError,'invalid argument'
         urllib.urlretrieve(url, filename)
     data=pandas.read_csv(filename, index_col=0).sort_index()
     # for i in range(data.columns.values.size):
