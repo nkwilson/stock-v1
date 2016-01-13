3727695cc1c026567ad6ca2ecdf220a90310bc20
Yubin <nkyubin@gmail.com>
2016-01-13 22:22:57 +0800

    insert stock code and name to signal

--- scan-all-stocks-for-candidate.py
+++ scan-all-stocks-for-candidate.py
@@ -8,8 +8,12 @@ count=stocks[0].count()
 for i in range(count):
     try:
         ret=stock_signal_w_new_find_candidate(stocks[0][i])
+        # df.insert(1, 'bar', df['one']) 插入一列
+        # df['one_trunc'] = df['one'][:2] 
         if not isinstance(ret, type(None)) :
-            print stocks[1][i],ret
+            ret.insert(0,'name', stocks[1][i])
+            ret.insert(1,'code', stocks[0][i])
+            print ret
     except ValueError, ve:
         continue
     except Exception, ex:
