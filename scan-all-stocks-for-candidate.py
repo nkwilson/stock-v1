5f3b01e7231c2bda7713064097501f176246f892
Yubin <nkyubin@gmail.com>
2016-01-14 07:46:32 +0800

    add progress printing and save data every loop

--- scan-all-stocks-for-candidate.py
+++ scan-all-stocks-for-candidate.py
@@ -9,18 +9,20 @@ count=stocks[0].count()
 data=pandas.DataFrame()
 for i in range(count):
     try:
+        print stocks[0][i],stocks[1][i]
         ret=stock_signal_w_new_find_candidate(stocks[0][i])
         # df.insert(1, 'bar', df['one']) 插入一列
         # df['one_trunc'] = df['one'][:2] 
         if not isinstance(ret, type(None)) :
             ret.insert(0,'name', stocks[1][i])
             ret.insert(1,'code', stocks[0][i])
-            pandas.concat([data, ret])
+            data=pandas.concat([data, ret])
+            data.to_csv('candidates.csv')
             print ret
     except ValueError, ve:
         continue
     except Exception, ex:
-        print stocks[0][i],stocks[1][i],ex
+        print stocks[0][i],ex
 
-data.to_csv('candidates.csv')
+
     
