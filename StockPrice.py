9f5c6833ecc884a539f11c4ea3676617ec86195e
Yubin <nkyubin@gmail.com>
2016-01-17 23:12:41 +0800

    adjust argument order

--- StockPrice.py
+++ StockPrice.py
@@ -3,6 +3,7 @@ import pandas
 import urllib
 import urllib2
 import os
+import datetime
 
 def StockPrice_old(stock):
     data=tushare.get_hist_data(stock, start='2015-01-01',end='2015-12-31')
@@ -43,10 +44,10 @@ def StockPrice_4(stock, type, start, end):
     return pandas.read_csv(f, index_col=0).sort_index()
 
 def StockPrice_w_3(stock, start, end):
-    return StockPrice_4(stock, start, end, 'w')
+    return StockPrice_4(stock, 'w', start, end)
 
 def StockPrice_w_2(stock, start):
-    return StockPrice_4(stock, start, pandas.datetime.now(), 'w')
+    return StockPrice_4(stock, 'w', start, pandas.datetime.now())
     
 def StockPrice_w(stock):
     url='http://real-chart.finance.yahoo.com/table.csv?s=%s&a=0&b=1&c=2015&d=11&e=11&f=2016&g=w&ignore=.csv' % stock
@@ -63,6 +64,39 @@ def StockPrice_w(stock):
 
     try:
         new_data=StockPrice_w_2(stock, pandas.datetime.strptime(start, '%Y-%m-%d'))
+    except Exception, ex:
+        return data
+
+    # pandas.concat([datal, new_data[1:]]) emit wrong message
+    new_data=new_data[1:]
+    data=pandas.concat([data, new_data])
+
+    # save to file using original order
+    data.sort_index(ascending=False).to_csv(filename)
+    
+    return data
+
+def StockPrice_d_3(stock, start, end):
+    return StockPrice_4(stock, 'd', start, end)
+
+def StockPrice_d_2(stock, start):
+    return StockPrice_4(stock, 'd', start, pandas.datetime.now())
+    
+def StockPrice_d(stock):
+    url='http://real-chart.finance.yahoo.com/table.csv?s=%s&a=0&b=1&c=2015&d=11&e=11&f=2016&g=d&ignore=.csv' % stock
+    filename='%sd.csv' % stock
+    if not os.path.isfile(filename):
+#        raise ValueError,'invalid argument'
+        urllib.urlretrieve(url, filename)
+    
+    data=pandas.read_csv(filename, index_col=0).sort_index()
+    start=data.index[data.index.size - 1]
+    end_dt=pandas.datetime.now()
+    if cmp(start, end_dt.strftime('%Y-%m-%d')) == 0:
+        return data
+
+    try:
+        new_data=StockPrice_d_2(stock, pandas.datetime.strptime(start, '%Y-%m-%d'))
     except Exception, ex:
         return data
 
