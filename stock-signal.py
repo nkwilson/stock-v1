2be19799a373d02801a36c6839ea969967d93556
Yubin <nkyubin@gmail.com>
2016-01-17 11:22:27 +0800

    filter those stocks that paused

--- stock-signal.py
+++ stock-signal.py
@@ -115,7 +115,7 @@ def stock_signal_w_new_find_candidate(stock):
     all_data=stock_signal_w_new(stock)
 
     count=all_data['signal'].count()
-    if all_data['signal'].sum() > 0:
+    if all_data['signal'].sum() > 0 and all_data['Volume'][count-1] > 0:
         for i in range(count-1, 0, -1):
             if all_data['signal'][i]==1:
                 return all_data.select(lambda x: True if x==all_data.index[i] else False)[['signal','Adj Close', 'EMA', 'buy', 'sell', 'profit']]
