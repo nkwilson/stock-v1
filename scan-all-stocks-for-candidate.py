5cd87e87062ec75953a26053dd4ca622823cfd72
Yubin <nkyubin@gmail.com>
2016-01-20 07:09:08 +0800

    use import to load modules

--- scan-all-stocks-for-candidate.py
+++ scan-all-stocks-for-candidate.py
@@ -1,16 +1,25 @@
 # load stock-signal.py # load it by manual
 # load load-code.py # load it by manual
+import StockSignal
+import LoadCode
 import time
 import random
 import pandas
 
-stocks=load_code_ex()
+stocks=LoadCode.load_code_ex()
 count=stocks[0].count()
 data=pandas.DataFrame()
+skip=1
+skip_before='600000.SS'
 for i in range(count):
     try:
+        if stocks[0][i].find(skip_before)==0:
+            skip=0
+        if skip == 1:
+            continue;
+        
         print stocks[0][i],stocks[1][i]
-        ret=stock_signal_w_new_find_candidate(stocks[0][i])
+        ret=StockSignal.stock_signal_w_new_find_candidate(stocks[0][i])
         # df.insert(1, 'bar', df['one']) 插入一列
         # df['one_trunc'] = df['one'][:2] 
         if not isinstance(ret, type(None)) :
@@ -23,6 +32,8 @@ for i in range(count):
         continue
     except Exception, ex:
         print stocks[0][i],ex
+        time.sleep(random.random()*10)
+
 if data.index.size > 0:
     data.sort_index().to_csv('candidates.csv')
 
--- scan-all-stocks.py
+++ scan-all-stocks.py
@@ -1,9 +1,11 @@
 # load stock-signal.py # load it by manual
 # load load-code.py # load it by manual
+import StockSignal
+import LoadCode
 import time
 import random
 
-stocks=load_code_ex()
+stocks=LoadCode.load_code_ex()
 count=stocks[0].count()
 left=count
 loop=0
@@ -13,7 +15,7 @@ while left > 0:
     loop=loop+1
     for i in range(count):
         try:
-            print stocks[1][i],stock_signal_w_new_simple(stocks[0][i])
+            print stocks[1][i],StockSignal.stock_signal_w_new_simple(stocks[0][i])
             left=left-1
         except ValueError, ve:
             left=left-1
