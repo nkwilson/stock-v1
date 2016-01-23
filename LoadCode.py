73d0deae2e11c9b4e0964654d02e98226fe93ad9
Yubin <nkyubin@gmail.com>
2016-01-23 23:19:24 +0800

    add load_etf_code()

--- LoadCode.py
+++ LoadCode.py
@@ -6,3 +6,5 @@ def load_code():
 def load_code_ex():
     return pandas.read_csv('stocks-code.ex-name.csv', header=None)
 
+def load_etf_code():
+    return pandas.read_csv('etf-funds.csv', header=None)
