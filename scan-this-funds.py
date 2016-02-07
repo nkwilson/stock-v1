546855117cac4eb6d6002a7b0b5925b9e27a66e0
yubin <yubin@>
2016-02-07 11:45:45 +0800

    add 518880/gold ETF

--- scan-this-funds.py
+++ scan-this-funds.py
@@ -3,7 +3,7 @@ import StockSignal
 
 funds=LoadCode.load_etf_code()
 
-ss_funds=[510010, 510050, 510210, 510290, 510500]
+ss_funds=[510010, 510050, 510210, 510290, 510300, 510500, 518880]
 for i in ss_funds: StockSignal.stock_signal_w_new_find_candidate('%s.SS' % i)
 
 sz_funds=[159915,159919, 159918, 159921, 159907, 159922]
