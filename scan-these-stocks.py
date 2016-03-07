c8cb45dcc8c5cfcb229a1f2a971b53eb38e38784
yubin <yubin@>
2016-03-07 08:37:09 +0800

    add focused stocks to check every week

---
+++ scan-these-stocks.py
@@ -0,0 +1,26 @@
+import StockSignal
+import pandas
+
+summary=None
+
+stocks=[['002673.SZ', 'xbzq'],
+          ['000973.SZ', 'fskj']]
+
+for i in stocks:
+    try:
+        ret = StockSignal.stock_signal_w_new_find_candidate(i[0])
+    except Exception, ex:
+        ret = None
+
+    if not isinstance(ret, type(None)) :
+        ret.insert(0,'name', i[1])
+        ret.insert(1,'code', i[0])
+        
+        if not isinstance(summary, type(None)):
+#            summary=pandas.DataFrame.append(summary,ret)
+            summary=pandas.concat([ret, summary])
+        else:
+            summary=ret
+
+if not isinstance(summary, type(None)):            
+    print summary.sort_index()
