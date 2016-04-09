cc7d772f7ec915d7f6bdc9e6dd5c79a36f3401f1
yubin <yubin@>
2016-04-09 22:41:37 +0800

    use pp to parallel computing

--- scan-all-funds-for-candidate.py
+++ scan-all-funds-for-candidate.py
@@ -2,31 +2,52 @@ import LoadCode
 import StockSignal
 import StockPrice
 import pandas
+import pp
 
 funds=LoadCode.load_etf_code()
 
 summary=None
 result_file='funds-candidates-%s.csv' % pandas.datetime.now().strftime('%Y-%m-%d')
-for i in range(funds['FundName'].count()):
-    try:
-        if funds['FundCode'][i] < 500000:
-            stock='%s.SZ' % funds['FundCode'][i]
-        else:
-            stock='%s.SS' % funds['FundCode'][i]
 
+def local_func(stock, code, name):
+    try:
         ret = StockSignal.stock_signal_w_new_find_candidate_with_volume(stock)
-    except Exception, ex:
-        ret = None
-    
-    if not isinstance(ret, type(None)):
-        ret.insert(0,'code', funds['FundCode'][i])
-        ret.insert(ret.columns.size,'name', funds['FundName'][i])
         
-        if not isinstance(summary, type(None)):
-            summary=pandas.concat([ret, summary])
-        else:
-            summary=ret
-            
+        ret.insert(0, 'code', code)
+        ret.insert(ret.columns.size, 'name', name)
+
+        return ret
+    except Exception, ex:
+        return None
+
+
+ppservers = ()
+jobs = []
+
+job_server = pp.Server(ppservers=ppservers)
+
+for i in range(funds['FundName'].count()):
+    if funds['FundCode'][i] < 500000:
+        stock='%s.SZ' % funds['FundCode'][i]
+    else:
+        stock='%s.SS' % funds['FundCode'][i]
+
+    jobs.append(job_server.submit(local_func, (stock, funds['FundCode'][i], funds['FundName'][i]), (), ("StockSignal","pandas", )))
+    
+for job in jobs:
+    ret = job()
+
+    if isinstance(ret, type(None)):
+        continue
+
+    print ret
+    
+    if not isinstance(summary, type(None)):
+        summary=pandas.concat([ret, summary])
+    else:
+        summary=ret
+
+
 if not isinstance(summary, type(None)):
     summary=summary.sort(['Volume'])
 #    summary=summary.sort_values(['code', 'Volume'])
