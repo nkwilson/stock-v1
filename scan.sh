878013a2c7db74b8c055e242118b969f0b2a07cf
Yubin <nkyubin@gmail.com>
2016-07-31 21:00:25 +0800

    fix bug of failed to find tusshare package
    if $OSTYPE is darwin15, then set PYTHONPATH environment argument

--- scan.sh
+++ scan.sh
@@ -1,2 +1,6 @@
-python scan-these-stocks.py
-python scan-this-funds.py
+if [ "${OSTYPE}" = "darwin15" ]; then
+export PYTHONPATH=/usr/local/lib/python2.7/site-packages
+fi
+
+python2.7 scan-these-stocks.py
+python2.7 scan-this-funds.py
