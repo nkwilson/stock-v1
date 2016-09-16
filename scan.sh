e460c6334c4a851b953c36573587eee92af8b1fd
Yubin <nkyubin@gmail.com>
2016-09-16 21:27:12 +0800

    fix last_fri bug and adapted for tacos

--- scan.sh
+++ scan.sh
@@ -7,8 +7,20 @@ python2.7 scan-this-funds.py   2>&1 > /dev/null # output: scan-this-funds-candid
 python2.7 scan-fenji-funds-for-candidate.py 2>&1 > /dev/null # output: fenji-funds-candidates-%Y-%m-%d.csv
 
 suffix=$(date +%Y-%m-%d)
-last_fri=$(date +%Y-%m-%d --date="Last Fri")
 
-python2.7 select-based-weekly-candidate.py select_base_weekly_candidate scan-these-stocks-candidates-${suffix}.csv "${last_fri}"
-python2.7 select-based-weekly-candidate.py select_base_weekly_candidate scan-this-funds-candidates-$suffix.csv "${last_fri}"
-python2.7 select-based-weekly-candidate.py select_base_weekly_candidate fenji-funds-candidates-$suffix.csv "${last_fri}"
+if [ "${OSTYPE}" = "darwin15" ]; then
+    last_fri=$(date -v-fri +%Y-%m-%d)
+else
+    last_fri=$(date +%Y-%m-%d --date="Fri")
+fi
+
+week_day=$(date +%w)
+last_key_day=$(date +%Y-%m-%d)
+
+if [ "${week_day}" -ge "5" ]; then
+    last_key_day=$last_fri
+fi
+
+python2.7 select-based-weekly-candidate.py select_base_weekly_candidate scan-these-stocks-candidates-${suffix}.csv "${last_key_day}"
+python2.7 select-based-weekly-candidate.py select_base_weekly_candidate scan-this-funds-candidates-$suffix.csv "${last_key_day}"
+python2.7 select-based-weekly-candidate.py select_base_weekly_candidate fenji-funds-candidates-$suffix.csv "${last_key_day}"
