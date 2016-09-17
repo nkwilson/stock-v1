05488d2c832e72fe3f061ec745feb19492ae4891
Yubin <nkyubin@gmail.com>
2016-09-17 22:49:58 +0800

    add total_cost to count the up limit of cost

--- new-weekly-policy.py
+++ new-weekly-policy.py
@@ -4,10 +4,14 @@ import numpy
 selling_good_deals=-1
 next_buy=-1
 global_tendency=0
-deal_cost=8000
+deal_cost=14000
+total_money=50000 # all of my money
+total_cost=0 # total cost of holding until now, must be less than total_money
 
 #data=pandas.read_csv('600547.SSw-all-data.csv', index_col=0).sort_index()
-data=pandas.read_csv('000799.SZw-all-data.csv', index_col=0).sort_index()
+#data=pandas.read_csv('000799.SZw-all-data.csv', index_col=0).sort_index()
+data=pandas.read_csv('000938.SZw-all-data.csv', index_col=0).sort_index()
+#data=pandas.read_csv('600519.SSw-all-data.csv', index_col=0).sort_index()
 
 lodgers=None
 
@@ -33,21 +37,27 @@ for i in range(data['signal'].count()):
        	   lodgers.loc[to_sold_deals.index[j]]['sell-date']=data.index[i]
 	   lodgers.loc[to_sold_deals.index[j]]['sell-price']=data['open'][i]
 	   lodgers.loc[to_sold_deals.index[j]]['profit']=(data['open'][i]-to_sold_deals['price'][j])*to_sold_deals['count'][j]
+           total_cost -= lodgers.loc[to_sold_deals.index[j]]['total']
+           
 #       print lodgers[['price','count','total','sell-date','sell-price']]
 
     selling_good_deals=-1
     
     if next_buy > 0:
-        new_row_data=pandas.DataFrame(index=data.index[i:i+1], columns=['price', 'count', 'total', 'signal', 'close_s', 'sell-date', 'sell-price', 'profit'])
+        new_row_data=pandas.DataFrame(index=data.index[i:i+1], columns=['price', 'count', 'total', 'total-cost', 'sell-date', 'sell-price', 'profit'])
         new_row_data['price'][0]=data['open'][i]
         new_row_data['count'][0]=int(deal_cost / data['open'][i]/100.0) * 100
         new_row_data['total'][0]=new_row_data['count'][0] * data['open'][i]
-        new_row_data['signal'][0]=0
-        new_row_data['close_s'][0]=0
+#        new_row_data['signal'][0]=0
+#        new_row_data['close_s'][0]=0
         new_row_data['sell-date'][0]=data.index[i]
         new_row_data['sell-price'][0]=0
         new_row_data['profit'][0]=0        
 
+        total_cost += new_row_data['total'][0]
+
+        new_row_data['total-cost'][0]=total_cost
+        
         if isinstance(lodgers, type(None)):
             lodgers=new_row_data
         else:
