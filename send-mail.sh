89eb9b483b8e34cea90e28f5a5005e5a50cf1435
nkwilson <nkwilson@sina.com>
2017-09-16 00:19:58 +0800

    add for debug

--- send-mail.sh
+++ send-mail.sh
@@ -1,7 +1,7 @@
-#! /bin/bash
+#!/bin/bash
 
 cat $1 | mailx -S'smtp=smtp.sina.com' -S'smtp-auth=login' -S'smtp-auth-user=nkwilson' -S'smtp-auth-password=1qaz2wsx' -r nkwilson@sina.com -s "Robot: $1" -. nkwilson@sina.com
 
 for i in *.pdf ; do
-    echo $i | mailx -S'smtp=smtp.sina.com' -S'smtp-auth=login' -S'smtp-auth-user=nkwilson' -S'smtp-auth-password=1qaz2wsx' -r nkwilson@sina.com -a $i -s "Robot: $i" -. nkwilson@sina.com
+    echo $i | mailx -S'smtp=smtp.sina.com' -S'smtp-auth=login' -S'smtp-auth-user=nkwilson' -S'smtp-auth-password=1qaz2wsx' -r nkwilson@sina.com -a $i -s "Robot: $i" -. nkwilson@sina.com && echo $i
 done
