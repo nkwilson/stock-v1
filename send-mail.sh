661a7a270e7424441bd8869588f5c9ccb17bb03c
nkwilson <nkwilson@sina.com>
2017-09-16 00:22:59 +0800

    seems quit too quickly

--- send-mail.sh
+++ send-mail.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cat $1 | mailx -S'smtp=smtp.sina.com' -S'smtp-auth=login' -S'smtp-auth-user=nkwilson' -S'smtp-auth-password=1qaz2wsx' -r nkwilson@sina.com -s "Robot: $1" -. nkwilson@sina.com
-
+sleep 1
 for i in *.pdf ; do
-    echo $i | mailx -S'smtp=smtp.sina.com' -S'smtp-auth=login' -S'smtp-auth-user=nkwilson' -S'smtp-auth-password=1qaz2wsx' -r nkwilson@sina.com -a $i -s "Robot: $i" -. nkwilson@sina.com && echo $i
+    echo $i | mailx -S'smtp=smtp.sina.com' -S'smtp-auth=login' -S'smtp-auth-user=nkwilson' -S'smtp-auth-password=1qaz2wsx' -r nkwilson@sina.com -a $i -s "Robot: $i" -. nkwilson@sina.com
+    sleep 1
 done
