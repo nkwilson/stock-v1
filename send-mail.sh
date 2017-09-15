7f0398b9f5602e0683721cc06875203a8566b4de
nkwilson <nkwilson@sina.com>
2017-09-15 23:49:02 +0800

    fix typo

--- send-mail.sh
+++ send-mail.sh
@@ -3,5 +3,5 @@
 cat $1 | mailx -S'smtp=smtp.sina.com' -S'smtp-auth=login' -S'smtp-auth-user=nkwilson' -S'smtp-auth-password=1qaz2wsx' -r nkwilson@sina.com -s "Robot: $1" -. nkwilson@sina.com
 
 for i in *.pdf ; do
-    cat $i | mailx -S'smtp=smtp.sina.com' -S'smtp-auth=login' -S'smtp-auth-user=nkwilson' -S'smtp-auth-password=1qaz2wsx' -r nkwilson@sina.com -a $i -s "Robot: $i" -. nkwilson@sina.com
+    echo $i | mailx -S'smtp=smtp.sina.com' -S'smtp-auth=login' -S'smtp-auth-user=nkwilson' -S'smtp-auth-password=1qaz2wsx' -r nkwilson@sina.com -a $i -s "Robot: $i" -. nkwilson@sina.com
 done
