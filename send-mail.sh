4b4c9cd780c561f88e7f6e8cdaa936180c255da7
nkwilson <nkyubin@gmail.com>
2017-09-19 14:12:29 +0000

    Set sendwait for mailx

--- send-mail.sh
+++ send-mail.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-cat $1 | mailx -S'smtp=smtp.sina.com' -S'smtp-auth=login' -S'smtp-auth-user=nkwilson' -S'smtp-auth-password=1qaz2wsx' -r nkwilson@sina.com -s "Robot: $1" -. nkwilson@sina.com
-sleep 1
+cat $1 | mailx -Ssendwait -S'smtp=smtp.sina.com' -S'smtp-auth=login' -S'smtp-auth-user=nkwilson' -S'smtp-auth-password=1qaz2wsx' -r nkwilson@sina.com -s "Robot: $1" -. nkwilson@sina.com
 for i in *.pdf ; do
     echo $i | mailx -S'smtp=smtp.sina.com' -S'smtp-auth=login' -S'smtp-auth-user=nkwilson' -S'smtp-auth-password=1qaz2wsx' -r nkwilson@sina.com -a $i -s "Robot: $i" -. nkwilson@sina.com
-    sleep 1
 done
