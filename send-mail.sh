a0060e02ef8b3b35f7070381eb37a1bb30bd640e
nkwilson <nkwilson@sina.com>
2017-09-19 22:18:54 +0800

    add missing send wait

--- send-mail.sh
+++ send-mail.sh
@@ -2,5 +2,5 @@
 
 cat $1 | mailx -Ssendwait -S'smtp=smtp.sina.com' -S'smtp-auth=login' -S'smtp-auth-user=nkwilson' -S'smtp-auth-password=1qaz2wsx' -r nkwilson@sina.com -s "Robot: $1" -. nkwilson@sina.com
 for i in *.pdf ; do
-    echo $i | mailx -S'smtp=smtp.sina.com' -S'smtp-auth=login' -S'smtp-auth-user=nkwilson' -S'smtp-auth-password=1qaz2wsx' -r nkwilson@sina.com -a $i -s "Robot: $i" -. nkwilson@sina.com
+    echo $i | mailx -Ssendwait -S'smtp=smtp.sina.com' -S'smtp-auth=login' -S'smtp-auth-user=nkwilson' -S'smtp-auth-password=1qaz2wsx' -r nkwilson@sina.com -a $i -s "Robot: $i" -. nkwilson@sina.com
 done
