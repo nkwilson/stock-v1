cc74e7cd564a40263c7174520a807c9923e30024
nkwilson <nkwilson@sina.com>
2017-09-24 21:13:39 +0800

    switch to smtps

--- send-mail.sh
+++ send-mail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat $1 | mailx -Ssendwait -S'smtp=smtp.sina.com' -S'smtp-auth=login' -S'smtp-auth-user=nkwilson' -S'smtp-auth-password=1qaz2wsx' -r nkwilson@sina.com -s "Robot: $1" -. nkwilson@sina.com
+cat $1 | mailx -Ssendwait -S'smtps=smtp.sina.com:587' -S'smtp-auth=login' -S'smtp-auth-user=nkwilson' -S'smtp-auth-password=1qaz2wsx' -r nkwilson@sina.com -s "Robot: $1" -. nkwilson@sina.com
 for i in *.pdf ; do
-    echo $i | mailx -Ssendwait -S'smtp=smtp.sina.com' -S'smtp-auth=login' -S'smtp-auth-user=nkwilson' -S'smtp-auth-password=1qaz2wsx' -r nkwilson@sina.com -a $i -s "Robot: $i" -. nkwilson@sina.com
+    echo $i | mailx -Ssendwait -S'smtps=smtp.sina.com:587' -S'smtp-auth=login' -S'smtp-auth-user=nkwilson' -S'smtp-auth-password=1qaz2wsx' -r nkwilson@sina.com -a $i -s "Robot: $i" -. nkwilson@sina.com
 done
