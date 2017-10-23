4bee6c144e910a84b7b366ab4ed98270dfe02ead
yubin <nkwilson@sina.com>
2017-10-23 10:02:33 +0800

    move bussiness logic to run-in-docker.sh

--- exec-new-container.sh
+++ exec-new-container.sh
@@ -37,7 +37,7 @@ RUN  locale-gen zh_CN.UTF-8
 ENV LANG=zh_CN.UTF-8
 ENV LC_ALL=zh_CN.UTF-8
      
-CMD git clone -b adx https://github.com/nkwilson/stock-v1.git && cd stock-v1 && git pull && bash -x run-in-docker.sh
+CMD bash -x run-in-docker.sh
 
 EOF
 
--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-bash core-work.sh > summary
-bash send-mail.sh summary
+# run stock-v1
+git clone -b adx https://github.com/nkwilson/stock-v1.git && cd stock-v1 && \
+ bash core-work.sh > summary && \
+ bash send-mail.sh summary
 
 
