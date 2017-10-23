d48151df6c4ad287e1980b8f2c85edd82e1325e6
yubin <nkwilson@sina.com>
2017-10-23 09:17:17 +0800

    do some reformat

--- exec-new-container.sh
+++ exec-new-container.sh
@@ -12,8 +12,15 @@ RUN  echo 'Asia/ShangHai' > /etc/timezone  && \
      sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list && \
      sed -i 's/security.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list && \
      apt-get update && \
-     apt-get -y install python-pip python-tk git command-not-found xvfb &&\
-     pip install --upgrade pip
+     apt-get -y install python-pip python-tk git command-not-found xvfb 
+
+RUN  apt-get -y install s-nail
+
+RUN  apt-get -y install locales
+
+RUN  apt-get autoclean && apt-get autoremove && apt-get clean
+
+RUN  pip install --upgrade pip
 
 RUN  pip install lxml && \
      pip install pandas
@@ -23,23 +30,14 @@ RUN  pip install requests && \
      pip install bs4 && \
      pip install tushare && \
      pip install pp && \
-     pip install matplotlib && \
-     git clone -b adx https://github.com/nkwilson/stock-v1.git
-
-RUN  apt-get -y install s-nail
-
-RUN  apt-get -y install locales
-
-RUN  apt-get autoclean && apt-get autoremove && apt-get clean
+     pip install matplotlib 
 
 RUN  locale-gen zh_CN.UTF-8
 
-RUN  cd stock-v1 && git log -1 --oneline > old && git pull && git log -1 --oneline > new && diff -q old new || touch file-$(date '+%S')
-
 ENV LANG=zh_CN.UTF-8
 ENV LC_ALL=zh_CN.UTF-8
-
-CMD cd stock-v1 && git pull && bash -x run-in-docker.sh
+     
+CMD git clone -b adx https://github.com/nkwilson/stock-v1.git && cd stock-v1 && git pull && bash -x run-in-docker.sh
 
 EOF
 
